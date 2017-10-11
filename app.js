8e64d602ac648959be1d6ec04386806132729d32
Tulio Navarro <tnavarrodesenvolvimento@gmail.com>
2017-10-11 16:18:12 -0300

    Alter env e app para bancos

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const app = express();
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGODB_URI);
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
