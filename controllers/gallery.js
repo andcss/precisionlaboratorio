362a0e1c0802c6481ab39311d97848dabd66c899
Tulio Navarro <tnavarrodesenvolvimento@gmail.com>
2018-02-26 19:26:09 -0300

    - [x] Adicionar slide na página home com possibilidade de alterar o conteúdo sendo ou videos do Youtube ou imagens.
    - [x] Capas para arquivos de formatos sem preview
    
    - [x] Adicionar vídeos do youtube na galeria

--- controllers/gallery.js
+++ controllers/gallery.js
@@ -56,7 +56,7 @@ exports.getMidia = (req, res) => {
 }
 
 exports.postNewMidia = (req, res) => {
-
+  if (req.files.fileUpdate.originalFilename != '') {
   cloudinary.v2.uploader.upload(req.files.fileUpdate.path,
     {
       resource_type: "auto",
@@ -67,34 +67,105 @@ exports.postNewMidia = (req, res) => {
         req.flash('errors', { msg: 'Um erro aconteceu no upload de arquivo.' })
         return res.redirect('/midia');
       }
-      Gallery.findOne({ name: 'Principal'}).populate('files._role').exec((err, gallery) => {
-        if (err) {
-          req.flash('errors', { msg: 'Não encontramos a galeria.' })
-          return res.redirect('/midia');
-        }
-        if (!gallery) {
-          req.flash('errors', { msg: 'Não encontramos a galeria.' })
-          return res.redirect('/midia');
-        }
 
-        gallery.files.push({
-          url: returnFileUpdate.secure_url,
-          type: returnFileUpdate.resource_type,
-          name: req.body.name,
-          format: returnFileUpdate.format,
-          description: req.body.description,
-          public_id: returnFileUpdate.public_id,
-          _role: req.body.role,
+      if (req.files.fileCapa) {
+        cloudinary.v2.uploader.upload(req.files.fileCapa.path,
+          {
+            resource_type: "auto",
+            public_id: "galeria/capas/"+req.files.fileCapa.name
+          },
+          function(err, returnFileCapa) {
+            Gallery.findOne({ name: 'Principal'}).populate('files._role').exec((err, gallery) => {
+              if (err) {
+                req.flash('errors', { msg: 'Não encontramos a galeria.' })
+                return res.redirect('/midia');
+              }
+              if (!gallery) {
+                var newGallery = new Gallery();
+                newGallery.name = 'Principal'
+                newGallery.save();
+                req.flash('errors', { msg: 'Não encontramos a galeria. Tente Novamente!' })
+                return res.redirect('/midia');
+              }
+
+              gallery.files.push({
+                url: returnFileUpdate.secure_url,
+                type: returnFileUpdate.resource_type,
+                name: req.body.name,
+                preview: returnFileCapa.secure_url,
+                format: returnFileUpdate.format,
+                description: req.body.description,
+                public_id: returnFileUpdate.public_id,
+                _role: req.body.role,
+              });
+
+              gallery.save((err, saveGallery) => {
+                req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
+                return res.redirect('/galleries');
+              })
+            });
+          }
+        );
+      } else {
+        Gallery.findOne({ name: 'Principal'}).populate('files._role').exec((err, gallery) => {
+          if (err) {
+            req.flash('errors', { msg: 'Não encontramos a galeria.' })
+            return res.redirect('/midia');
+          }
+          if (!gallery) {
+            var newGallery = new Gallery();
+            newGallery.name = 'Principal'
+            newGallery.save();
+            req.flash('errors', { msg: 'Não encontramos a galeria. Tente Novamente!' })
+            return res.redirect('/midia');
+          }
+
+          gallery.files.push({
+            url: returnFileUpdate.secure_url,
+            type: returnFileUpdate.resource_type,
+            name: req.body.name,
+            format: returnFileUpdate.format,
+            description: req.body.description,
+            public_id: returnFileUpdate.public_id,
+            _role: req.body.role,
+          });
+
+          gallery.save((err, saveGallery) => {
+            req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
+            return res.redirect('/galleries');
+          })
         });
+      }
 
-        gallery.save((err, saveGallery) => {
-          req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
-          return res.redirect('/galleries');
-        })
+
+    });
+  } else {
+    Gallery.findOne({ name: 'Principal'}).populate('files._role').exec((err, gallery) => {
+      if (err) {
+        req.flash('errors', { msg: 'Não encontramos a galeria.' })
+        return res.redirect('/midia');
+      }
+      if (!gallery) {
+        var newGallery = new Gallery();
+        newGallery.name = 'Principal'
+        newGallery.save();
+        req.flash('errors', { msg: 'Não encontramos a galeria. Tente Novamente!' })
+        return res.redirect('/midia');
+      }
+
+      gallery.files.push({
+        codeYoutube: req.body.codeYoutube,
+        name: req.body.name,
+        description: req.body.description,
+        _role: req.body.role,
       });
-    }
-  );
 
+      gallery.save((err, saveGallery) => {
+        req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
+        return res.redirect('/galleries');
+      })
+    });
+  }
 
 }
 
@@ -116,27 +187,61 @@ exports.postMidia = (req, res) => {
             return res.redirect('/midia');
           }
           if (!gallery) {
-            req.flash('errors', { msg: 'Não encontramos a galeria.' })
+            var newGallery = new Gallery();
+            newGallery.name = 'Principal'
+            newGallery.save();
+            req.flash('errors', { msg: 'Não encontramos a galeria. Tente Novamente!' })
             return res.redirect('/midia');
+
           }
 
-          gallery.files = gallery.files.map((file) => {
-            if (file._id == req.params.id) {
-              file.url = returnFileUpdate.secure_url;
-              file.type = returnFileUpdate.resource_type;
-              file.name = req.body.name;
-              file.format = returnFileUpdate.format;
-              file.description = req.body.description;
-              file.public_id = returnFileUpdate.public_id;
-              file._role = req.body.role;
-            }
-            return file;
-          });
+          if (req.files.fileCapa.originalFilename != '') {
+            cloudinary.v2.uploader.upload(req.files.fileCapa.path,
+              {
+                resource_type: "auto",
+                public_id: "galeria/capas/"+req.files.fileCapa.name
+              },
+              function(err, returnFileCapa) {
+                gallery.files = gallery.files.map((file) => {
+                  if (file._id == req.params.id) {
+                    file.url = returnFileUpdate.secure_url;
+                    file.type = returnFileUpdate.resource_type;
+                    file.name = req.body.name;
+                    file.preview = returnFileCapa.secure_url;
+                    file.format = returnFileUpdate.format;
+                    file.description = req.body.description;
+                    file.public_id = returnFileUpdate.public_id;
+                    file._role = req.body.role;
+                  }
+                  return file;
+                });
 
-          gallery.save((err, saveGallery) => {
-            req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
-            return res.redirect('/galleries');
-          });
+                gallery.save((err, saveGallery) => {
+                  req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
+                  return res.redirect('/galleries');
+                });
+              }
+            );
+          } else {
+            gallery.files = gallery.files.map((file) => {
+              if (file._id == req.params.id) {
+                file.codeYoutube = '';
+                file.url = returnFileUpdate.secure_url;
+                file.type = returnFileUpdate.resource_type;
+                file.name = req.body.name;
+                file.format = returnFileUpdate.format;
+                file.description = req.body.description;
+                file.public_id = returnFileUpdate.public_id;
+                file._role = req.body.role;
+              }
+              return file;
+            });
+
+            gallery.save((err, saveGallery) => {
+              req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
+              return res.redirect('/galleries');
+            });
+          }
         });
       }
     );
@@ -147,23 +252,52 @@ exports.postMidia = (req, res) => {
         return res.redirect('/midia');
       }
       if (!gallery) {
-        req.flash('errors', { msg: 'Não encontramos a galeria.' })
+        var newGallery = new Gallery();
+        newGallery.name = 'Principal'
+        newGallery.save();
+        req.flash('errors', { msg: 'Não encontramos a galeria. Tente Novamente!' })
         return res.redirect('/midia');
       }
+      if (req.files.fileCapa.originalFilename != '') {
 
-      gallery.files = gallery.files.map((file) => {
-        if (file._id == req.params.id) {
-          file.name = req.body.name;
-          file.description = req.body.description;
-          file._role = req.body.role;
-        }
-        return file;
-      });
+        cloudinary.v2.uploader.upload(req.files.fileCapa.path,
+          {
+            resource_type: "auto",
+            public_id: "galeria/capas/"+req.files.fileCapa.name
+          },
+          function(err, returnFileCapa) {
+            gallery.files = gallery.files.map((file) => {
+              if (file._id == req.params.id) {
+                file.name = req.body.name;
+                file.preview = returnFileCapa.secure_url;
+                file.description = req.body.description;
+                file._role = req.body.role;
+              }
+              return file;
+            });
 
-      gallery.save((err, saveGallery) => {
-        req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
-        return res.redirect('/galleries');
-      });
+            gallery.save((err, saveGallery) => {
+              req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
+              return res.redirect('/galleries');
+            });
+          }
+        );
+      } else {
+        gallery.files = gallery.files.map((file) => {
+          if (file._id == req.params.id) {
+            file.name = req.body.name;
+            file.codeYoutube = req.body.codeYoutube;
+            file.description = req.body.description;
+            file._role = req.body.role;
+          }
+          return file;
+        });
+
+        gallery.save((err, saveGallery) => {
+          req.flash('success', { msg: 'Novo arquivo adicionado a galeria.' })
+          return res.redirect('/galleries');
+        });
+      }
     });
   }
 
--- models/Gallery.js
+++ models/Gallery.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const mongoosePaginate = require('mongoose-paginate');
 
 const gallerySchema = new Schema({
   name: { type: String, default: ''},
@@ -8,12 +9,15 @@ const gallerySchema = new Schema({
     url: { type: String, default: ''},
     type: { type: String, default: ''},
     name: { type: String, default: ''},
+    codeYoutube: { type: String, default: ''},
+    preview: { type: String, default: ''},
     description: { type: String, default: ''},
     public_id: { type: String, default: ''},
     format: { type: String, default: ''},
     _role: { type: Schema.Types.ObjectId, ref: 'Role' },
   }]
 }, { timestamps: true });
+gallerySchema.plugin(mongoosePaginate);
 
 const Gallery = mongoose.model('Gallery', gallerySchema);
 
